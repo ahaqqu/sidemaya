04fbff40ef34f233cd9c3ffc7a167c5ba0e03e91
adipyoga <work.yapratama@gmail.com>
2024-03-11 17:50:56 +0700

    coba lagi

--- sidemaya-website/app/Http/Controllers/AjukanSuratController.php
+++ sidemaya-website/app/Http/Controllers/AjukanSuratController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Document;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AjukanSuratController extends Controller
 {
-
     public function upload(Request $request)
     {
         // Validate the uploaded file
         $request->validate([
             'file' => 'required|file|mimes:pdf,doc,docx|max:10240',
             'nomorsurat' => 'required|string',
+            'uuid' => 'required|string', // Tambahkan validasi untuk uuid
         ]);
 
         // Process the uploaded file
@@ -26,24 +25,26 @@ class AjukanSuratController extends Controller
             $nomorsurat = $request->input('nomorsurat');
             $uuid = $request->input('uuid');
 
+            // Validate UUID and retrieve the document
             $document = Document::where('uuid', '=', $uuid)->firstOrFail();
 
             $originalname = $file->getClientOriginalName();
-            $extension = substr($originalname, strrpos($originalname, '.')+1);
+            $extension = $file->getClientOriginalExtension();
             $fileName = $uuid . "." . $extension;
-            $category = $document->category;
+
+            // Store the file
             $file->storeAs("files/final", $fileName, 'private');
 
+            // Update the document
             $document->identifier = $nomorsurat;
             $document->status = "Selesai";
-            $document->updated_at = Carbon::now()->format('Y-m-d H:i:s');
+            $document->updated_at = Carbon::now();
             $document->updated_by = Auth::user()->id;
-            $document->update();
+            $document->save();
 
             return redirect()->back()->with('success', "Dokumen sukses diunggah.");
         } else {
             return redirect()->back()->with('error', 'Dokumen gagal diunggah. Sistem hanya mendukung tipe dokumen: doc, docx, dan pdf');
         }
-
     }
 }
