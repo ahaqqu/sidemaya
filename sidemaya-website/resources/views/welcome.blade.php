65219eb6eb375716b77bc622eeabd432f3a690fe
Angga <angga.bariesta@gmail.com>
2024-03-24 17:57:37 +0700

    Merapikan UI

--- sidemaya-website/resources/views/welcome.blade.php
+++ sidemaya-website/resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
 
     <x-slot name="header">
+    <div class="container w-full md:w-4/5 xl:w-3/5  mx-auto px-2" >
                 <!-- Landing Page Section -->
-                <div class="landing-page" style="background-image: url('backgrounds/your-background-image.jpg'); height: 100vh; display: flex; justify-content: center; align-items: center; text-align: center;">
-            <div class="container">
+            <div class="container" style="margin-top:200px">
                 <div class="jumbotron text-center p-5">
-                    <h1 class="text-white font-bold text-4xl md:text-6xl">Selamat Datang di Layanan SIDEMAYA</h1>
-                    <p class="text-white mt-4 text-lg md:text-xl">Solusi terpadu yang dirancang untuk mendukung pemerintahan desa, meningkatkan partisipasi masyarakat, dan memfasilitasi berbagai kegiatan di tingkat desa.</p>
+                    <h1 class="text-white font-bold text-4xl md:text-6xl" style="text-shadow: 2px 2px black;">Selamat Datang di Layanan SIDEMAYA</h1>
+                    <p class="text-white mt-4 text-4xl md:text-xl" style="text-shadow: 2px 2px black;">Solusi terpadu yang dirancang untuk mendukung pemerintahan desa, meningkatkan partisipasi masyarakat, dan memfasilitasi berbagai kegiatan di tingkat desa.</p>
                     <div class="mt-8">
                         <!-- Button untuk menarik perhatian pengguna, misalnya untuk mengarahkan mereka ke bagian tertentu dari situs -->
                         <a href="#tentangkami" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">Tentang Kami</a>
@@ -18,6 +18,7 @@
 
         <head>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    </div>
     </head>
 
 </x-app-layout>
